656239847a147759a9f8f1f30dbb45f1ef4e41ae
ksp0814 <rkdtpvlf456@naver.com>
2024-10-01 12:42:20 +0900

    프로그래밍 요구사항 반영

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,13 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Application {
@@ -12,19 +16,11 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
-        Random random = new Random(); // 랜덤 함수 지정
-        int target; // 타겟을 설정
-
-        while (true) {
-            target = random.nextInt(100,1000);
-
-            int first = target / 100;
-            int second = (target / 10) % 10;
-            int third = target % 10;
-
-            if (first != second && first != third && second != third) {
-                break;
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
             }
         }
 
@@ -38,26 +34,21 @@ public class Application {
             int strike = 0;
             int ball = 0;
 
-            int first_target = target / 100; // 100 자릿수
-            int second_target = (target / 10) % 10; // 10의 자릿수
-            int third_target = target % 10; // 1의 자릿수
-
-            int[] target_arr = {first_target, second_target, third_target}; // 타겟 각 자릿수의 값을 리스트로 생성
-            System.out.println("target_arr = " + Arrays.toString(target_arr));
 
             int first_number = number / 100; // 100 자릿수
             int second_number = (number / 10) % 10; // 10의 자릿수
             int third_number = number % 10; // 1의 자릿수
 
             int[] number_arr = {first_number, second_number, third_number}; // 입력 각 자릿수의 값을 리스트화
+            System.out.println("computer = "+computer);
             System.out.println("number_arr = " + Arrays.toString(number_arr));
 
             for (int i = 0; i < number_arr.length; i++) {  // 각 자리수 비교하고 strike ball 추가하는 로직
                 for (int j = 0; j < number_arr.length; j++) {
-                    if (target_arr[i] == number_arr[j] && i == j) {
+                    if (computer.get(i) == number_arr[j] && i == j) {
                         strike++;
                         break;
-                    } else if (target_arr[i] == number_arr[j]) {
+                    } else if (computer.get(i) == number_arr[j]) {
                         ball++;
                     }
                 }
@@ -72,15 +63,11 @@ public class Application {
                 int ans = Integer.parseInt(br.readLine());
                 if (ans == 1) {
                     // 여기에 새로 게임을 시작하는 코드를 작성하면 될듯
-                    while (true) {
-                        target = random.nextInt(100, 1000);
-
-                        int first = target / 100;
-                        int second = (target / 10) % 10;
-                        int third = target % 10;
-
-                        if (first != second && first != third && second != third) {
-                            break;
+                    computer.clear();
+                    while (computer.size() < 3) {
+                        int randomNumber = Randoms.pickNumberInRange(1, 9);
+                        if (!computer.contains(randomNumber)) {
+                            computer.add(randomNumber);
                         }
                     }
                 }else if (ans == 2) {
